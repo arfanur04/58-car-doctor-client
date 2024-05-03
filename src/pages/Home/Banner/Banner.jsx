bcfd5f9731124496528e04568503161ace401e8b
arfanur04 <arfanur04@gmail.com>
2024-05-03 15:52:12 +0600

    58-4 Style Banner Component With Gradient Overlay

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -2,26 +2,49 @@ import img1 from "../../../assets/images/banner/1.jpg";
 import img2 from "../../../assets/images/banner/2.jpg";
 import img3 from "../../../assets/images/banner/3.jpg";
 import img4 from "../../../assets/images/banner/4.jpg";
-import BannerNavigation from "./BannerNavigation";
-// import img5 from "../../../assets/images/banner/5.jpg";
-// import img6 from "../../../assets/images/banner/6.jpg";
 
 const Banner = () => {
 	return (
-		<div className="w-full carousel">
+		<div className="w-full carousel h-[600px]">
 			<div
 				id="slide1"
 				className="relative w-full carousel-item"
 			>
 				<img
 					src={img1}
-					className="w-full"
+					className="w-full rounded-lg"
 				/>
-
-				<BannerNavigation
-					prevSlide={"slide4"}
-					nextSlide={"slide2"}
-				></BannerNavigation>
+				<div className="absolute flex items-center h-full bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0) 100%)]">
+					<div className="w-1/2 pl-12 text-white space-y-7">
+						<h2 className="text-6xl font-bold">
+							Affordable Price For Car Servicing
+						</h2>
+						<p>
+							There are many variations of passages of available, but the
+							majority have suffered alteration in some form
+						</p>
+						<div>
+							<button className="mr-5 btn btn-primary">Discover More</button>
+							<button className="btn btn-outline btn-secondary">
+								Latest Project
+							</button>
+						</div>
+					</div>
+				</div>
+				<div className="absolute bottom-0 flex justify-end transform -translate-y-1/2 left-5 right-5">
+					<a
+						href="#slide4"
+						className="mr-5 btn btn-circle"
+					>
+						❮
+					</a>
+					<a
+						href="#slide2"
+						className="btn btn-circle"
+					>
+						❯
+					</a>
+				</div>
 			</div>
 			<div
 				id="slide2"
@@ -29,13 +52,22 @@ const Banner = () => {
 			>
 				<img
 					src={img2}
-					className="w-full"
+					className="w-full rounded-lg"
 				/>
-
-				<BannerNavigation
-					prevSlide={"slide1"}
-					nextSlide={"slide3"}
-				></BannerNavigation>
+				<div className="absolute bottom-0 flex justify-end transform -translate-y-1/2 left-5 right-5">
+					<a
+						href="#slide1"
+						className="mr-5 btn btn-circle"
+					>
+						❮
+					</a>
+					<a
+						href="#slide3"
+						className="btn btn-circle"
+					>
+						❯
+					</a>
+				</div>
 			</div>
 			<div
 				id="slide3"
@@ -43,12 +75,22 @@ const Banner = () => {
 			>
 				<img
 					src={img3}
-					className="w-full"
+					className="w-full rounded-lg"
 				/>
-				<BannerNavigation
-					prevSlide={"slide2"}
-					nextSlide={"slide4"}
-				></BannerNavigation>
+				<div className="absolute bottom-0 flex justify-end transform -translate-y-1/2 left-5 right-5">
+					<a
+						href="#slide2"
+						className="mr-5 btn btn-circle"
+					>
+						❮
+					</a>
+					<a
+						href="#slide4"
+						className="btn btn-circle"
+					>
+						❯
+					</a>
+				</div>
 			</div>
 			<div
 				id="slide4"
@@ -56,12 +98,22 @@ const Banner = () => {
 			>
 				<img
 					src={img4}
-					className="w-full"
+					className="w-full rounded-lg"
 				/>
-				<BannerNavigation
-					prevSlide={"slide3"}
-					nextSlide={"slide1"}
-				></BannerNavigation>
+				<div className="absolute bottom-0 flex justify-end transform -translate-y-1/2 left-5 right-5">
+					<a
+						href="#slide3"
+						className="mr-5 btn btn-circle"
+					>
+						❮
+					</a>
+					<a
+						href="#slide1"
+						className="btn btn-circle"
+					>
+						❯
+					</a>
+				</div>
 			</div>
 		</div>
 	);
